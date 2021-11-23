85e5936a6e5bb560855b1020187f38094a84a15f
jvschlierf <67417254+jvschlierf@users.noreply.github.com>
2021-11-23 22:53:07 +0100

    Update Darknet_Util_yolov4.py

--- project_01/Darknet_Util_yolov4.py
+++ project_01/Darknet_Util_yolov4.py
@@ -1,8 +1,21 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                Bocconi University                                     #
+#                       20600: Deep Learning for Computer Vision                        #
+#                                   Group JARVIS                                        #
+#                                  Darknet Utility                                      #
+#                                                                                       #
+# This program seeks to simplify the import and preparation steps for the use of darknet#
+# It takes the unzipped Roboflow folders, places the files into the darknet structure   #
+# and lastly creates the required data.txt files that will be used for training.        #
+#                                                                                       #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
 import os
 import pandas as pd
 import shutil
 
-Folders = [name for name in os.listdir() if os.path.isdir(os.path.join("", name)) and name[0] != "."]
+Folders = [name for name in os.listdir(os.getcwd()) if os.path.isdir(os.path.join("", name)) and name[0] != "."]
 #since we copy our images into the darknet folder structure, we check if darknet is present
 try:
     Folders.remove("darknet")
@@ -10,9 +23,9 @@ except ValueError:
     raise Exception("Please clone darknet into this folder.")
 
 try:
-    os.mkdir("darknet/build/darknet/x64/data/obj/")
-    os.mkdir("darknet/build/darknet/x64/data/obj/test/")
-    os.mkdir("darknet/build/darknet/x64/data/obj/valid/")
+    os.mkdir("darknet/data/train/")
+    os.mkdir("darknet/data/test/")
+    os.mkdir("darknet/data/valid/")
 except FileExistsError:
     pass
 
@@ -25,9 +38,9 @@ names_bool = True
 
 for i in Folders:
     #remove roboflow descriptors
-    os.remove(i + "README.dataset.txt")
-    os.remove(i + "README.dataset.txt")
-    subfolders = [name for name in os.listdir() if os.path.isdir(os.path.join("", name)) and name[0] != "."]
+    # os.remove(i + "/README.dataset.txt")
+    # os.remove(i + "/README.dataset.txt")
+    subfolders = [name for name in os.listdir(i) if os.path.isdir(os.path.join(i, name)) and name[0] != "."]
 
     #We check if we have a test subfolder to distinguish between a regular set (containing all 15 classes, train,
     # validate & test data and an augmented set (containing only train data, and not for all classes)
@@ -36,29 +49,49 @@ for i in Folders:
         temp_valid += [f for f in os.listdir(i + "/valid") if f.endswith(".jpg")]
         temp_test += [f for f in os.listdir(i + "/test") if f.endswith(".jpg")]
 
-        [shutil.move(i + "/test/" + f, "darknet/build/darknet/x64/data/obj/test/") for f in os.listdir(i + "/test/")]
-        [shutil.move(i + "/valid/" + f, "darknet/build/darknet/x64/data/obj/test/") for f in os.listdir(i + "/valid/")]
-        subfolders.remove("test")
-        subfolders.remove("valid")
         # we try to move the label files to the function specified by darknet
         if names_bool:
-            os.rename(i + "/test/_darknet.labels", "darknet/build/darknet/x64/data/obj.names")
+            os.rename(i + "/test/_darknet.labels", "darknet/data/obj.names")
             names_bool = False
+        
+        try:
+            os.remove(i + "/test/" + "_darknet.labels")
+            os.remove(i + "/valid/" + "_darknet.labels")
+        except:
+            pass
+
+        [shutil.move(i + "/test/" + f, "darknet/data/test/") for f in os.listdir(i + "/test/")]
+        [shutil.move(i + "/valid/" + f, "darknet/data/valid/") for f in os.listdir(i + "/valid/")]
+        
+        subfolders.remove("test")
+        subfolders.remove("valid")
+
+        os.rmdir(i + "/valid")
+        os.rmdir(i + "/test")
+
 
     for j in subfolders:
-        os.remove(i + "/" + j + "/" + "_darknet.labels")
-        [shutil.move(i + "/" + j + "/" + f, "darknet/build/darknet/x64/data/obj/") for f in os.listdir(i + "/" + j)]
+        try:
+            os.remove(i + "/" + j + "/" + "_darknet.labels")
+        except:
+            pass
+        [shutil.move(i + "/" + j + "/" + f, "darknet/data/train/") for f in os.listdir(i + "/" + j)]
 
         try:
             os.rmdir(i + "/" + j)
         except OSError:
             pass
+    
+    try:
+            os.rmdir(i)
+    except OSError:
+            pass
 
 
 # add the path we are working in
-train_loc = [os.getcwd() + "darknet/build/darknet/x64/data/obj/" + f for f in temp_train]
-valid_loc = [os.getcwd() + "darknet/build/darknet/x64/data/obj/valid/" + f for f in temp_valid]
-test_loc = [os.getcwd() + "darknet/build/darknet/x64/data/obj/test/" + f for f in temp_test]
+train_loc = [os.getcwd() + "darknet/data/train/" + f for f in temp_train]
+valid_loc = [os.getcwd() + "darknet/data/valid/" + f for f in temp_valid]
+test_loc = [os.getcwd() + "darknet/data/test/" + f for f in temp_test]
 
 # convert to pandas
 train_df = pd.DataFrame(train_loc)
@@ -68,6 +101,6 @@ test_df = pd.DataFrame(test_loc)
 # export the files in the destination folder
 # make sure you set index=False and header=False as the txt files
 # need to have just locations and nothing else
-train_df.to_csv("darknet/build/darknet/x64/data/train.txt", index=False, header=False)
-valid_df.to_csv("darknet/build/darknet/x64/data/valid.txt", index=False, header=False)
-test_df.to_csv("darknet/build/darknet/x64/data/test.txt", index=False, header=False)
+train_df.to_csv("darknet/data/train.txt", index=False, header=False)
+valid_df.to_csv("darknet/data/valid.txt", index=False, header=False)
+test_df.to_csv("darknet/data/test.txt", index=False, header=False)
